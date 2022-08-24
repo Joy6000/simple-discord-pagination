07ac794655b6d85be604eb15bbbaa3b322ae9466
Joy6000 <fnafgamer545@gmail.com>
2022-08-24 21:48:28 +0200

    Add button functionality

--- README.md
+++ README.md
@@ -8,25 +8,39 @@ npm install simpler-discord-pagination
 ```ts
     import { pagination } from 'simpler-discord-pagination'
     const embed1 = {
-        title = 'One'
+        title: 'One'
     }
     const embed2 = {
-        title = 'Twi'
+        title: 'Two'
     }
 
-    new pagination(embeds: [embed1, embed2], channel).newPagination() // channel is a textchannel object
+    new pagination({
+        embeds: [embded1, embed2],
+        channel: {}, // Channel Object Goes Here,
+        emojis: [],// There is no need to use this. Only if you want to change them. The emojis are built into the package.
+        timeout: 60000,// Must be an Integer. No need for this either. It's built in too. Change it if you want.
+        buttons: Boolean, //True or False goes here. Automatic to false.
+        client: Discord-Client // please pass in your discord.js client to make use of buttons.
+    }).newPagination() 
 ```
 ### JavaScript
 ```js
     const { pagination } = require('simpler-discord-pagination')
     const embed1 = {
-        title = 'One'
+        title: 'One'
     }
     const embed2 = {
-        title = 'Two'
+        title: 'Two'
     }
 
-    new pagination(embeds: [embed1, embed2], channel).newPagination() // channel is a textchannel object
+    new pagination({
+        embeds: [embded1, embed2],
+        channel: {}, // Channel Object Goes Here.
+        emojis: [],// There is no need to use this. Only if you want to change them. The emojis are built into the package.
+        timeout: 60000,// Must be an Integer. No need for this either. It's built in too. Change it if you want.
+        buttons: Boolean, //True or False goes here. Automatic to false. 
+        client: Discord-Client // please pass in your discord.js client to make use of buttons.
+    }).newPagination() 
 ```
 # Why use simpler-discord-pagination?
 simpler-discord-pagination simplified the traditional discord-pagination. We did this by utilizing a class, which is essentially a fancy function.
---
+++ index.js
@@ -0,0 +1,82 @@
+const {embedLength} = require("discord.js");
+
+class pagination {
+    constructor({
+                    embeds = [], channel = {}, emojis = ['⬅', '➡'], timeout = 60000, buttons = false, Discord = {}, client = {}}) {
+        this.embeds = embeds
+        this.channel = channel
+        this.emojis = emojis
+        this.timeout = timeout
+        this.buttons = buttons
+        this.Discord = Discord
+        this.client = client
+    }
+    async newPaginate() {
+        try {
+            const {embeds, channel, emojis, timeout, buttons, Discord, client} = this
+            if (buttons === false) {
+                const msg = await channel.send({embeds: [embeds[0]]})
+                emojis.forEach(emoji => msg.react(emoji))
+                let currentPage = 0;
+                const filter = (reaction, user) => {
+                    return reaction.emoji.name === emojis[0] && !user.bot || reaction.emoji.name === emojis[1] && !user.bot
+                }
+                const collector = await msg.createReactionCollector({filter, time: timeout})
+                collector.on('collect', (reaction, user) => {
+                    reaction.users.remove(user)
+                    switch (true) {
+                        case reaction.emoji.name === emojis[1]:
+                            currentPage = currentPage + 1
+                            break;
+                        case reaction.emoji.name === emojis[0]:
+                            currentPage = currentPage - 1
+                            break;
+
+                    }
+                    if (currentPage === -1) currentPage = embeds.length
+                    if (currentPage === embeds.length) currentPage = 0
+                    console.log(currentPage)
+                    msg.edit({embeds: [embeds[currentPage]]})
+                })
+            } else if (buttons) {
+                let currentPage = 0;
+                const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
+                const buttons = [
+                    new ButtonBuilder()
+                        .setCustomId('back')
+                        .setLabel('Back')
+                        .setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder()
+                        .setCustomId('forward')
+                        .setLabel('Forward')
+                        .setStyle(ButtonStyle.Primary)
+
+                ]
+
+                const row = new ActionRowBuilder().addComponents(buttons)
+                const msg = await channel.send({ embeds: embeds[0], components: [row] })
+
+                client.on('interactionCreate', (int) => {
+                    if (!int.isButton() && !int.message.author.id !== client.id) return
+                    if (int.customId === 'back') {
+                        if (currentPage === 0) return
+                        if (currentPage > 0) {
+                            currentPage = currentPage - 1
+                        }
+                    }
+                    if (int.customId === 'forward') {
+                        if (currentPage === embeds.length) return
+                        if (currentPage < embeds.length) {
+                            currentPage = currentPage + 1
+                        }
+                    }
+                    int.message.edit({embeds: [embeds[currentPage]], components: [row]})
+                })
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+}
+module.exports.pagination = pagination
